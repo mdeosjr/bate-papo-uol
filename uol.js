a0a6ced8f026433e27313611a51faafabbb3a4e8
Moises Junior <m.junior@eq.ufrj.br>
2021-11-10 16:42:03 -0300

    innerHTML modificado com sucesso e tentativa de adição de classe a mensagem

--- uol.js
+++ uol.js
@@ -1,15 +1,31 @@
-let participantes = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants");
-let status = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status");
-let buscarMensagens = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
-let enviarMensagens = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
-let buscarParticipantes = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants");
+let participantes = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants");
+let status = axios.post("https://mock-api.driven.com.br/api/v4/uol/status");
+let buscarMensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+let enviarMensagens = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages");
+let buscarParticipantes = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
 
 buscarMensagens.then(servidorMensagens)
 
 function servidorMensagens(mensagem) {
-    const chat = document.querySelector(".mensagem")
-    console.log(mensagem.data)
+    const chat = document.querySelector(".chat")
+    const tipo = document.querySelector(".mensagem")
+    //let comparador = mensagem.data.type
 
-    chat.innerHTML = `${mensagem.data.time} ${mensagem.data.from} ${mensagem.data.text}`
+    for (let i = 0; i < 7; i++) {
+        chat.innerHTML += 
+        `
+        <div class="mensagem">
+            (${mensagem.data[i].time}) ${mensagem.data[i].from} ${mensagem.data[i].text} ${mensagem.data[i].type}
+        </div>
+        `
+    }
+
+    if (mensagem.data[i].type.includes('status')) {
+        tipo.classList.add("status")
+    } else if (mensagem.data[i].type === 'private_message') {
+        tipo.classList.add("reservado")
+    } else {
+        tipo.classList.add("normal")
+    }
 }
 
