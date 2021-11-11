39a757b1ef54f1af9e53ad42d9d28e05ec635230
Moises Junior <m.junior@eq.ufrj.br>
2021-11-11 00:54:17 -0300

    Entrando na sala e enviando mensagem

--- uol.js
+++ uol.js
@@ -1,31 +1,54 @@
-let participantes = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants");
-let status = axios.post("https://mock-api.driven.com.br/api/v4/uol/status");
+//let status = axios.post("https://mock-api.driven.com.br/api/v4/uol/status");
+//let buscarParticipantes = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+
+let nome = prompt("Qual é o seu nome?")
+let dadosNome = {name: nome}
+let participantes = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", dadosNome);
+participantes.then(entrarNaSala)
+participantes.catch(erroDeNome)
+
+function entrarNaSala(acerto) {
+    //console.log(acerto.data)
+}
+
+function erroDeNome(erro) {
+    //console.log(erro.response.data)
+    alert("Já existe um participante com este nome, por favor digite outro")
+    nome = prompt("Qual é o seu nome?")
+}
+
+let chat = document.querySelector(".chat")
+let mensagem = document.querySelector(".mensagem")
 let buscarMensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-let enviarMensagens = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages");
-let buscarParticipantes = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
-
-buscarMensagens.then(servidorMensagens)
-
-function servidorMensagens(mensagem) {
-    const chat = document.querySelector(".chat")
-    const tipo = document.querySelector(".mensagem")
-    //let comparador = mensagem.data.type
-
-    for (let i = 0; i < 7; i++) {
+buscarMensagens.then(verMensagens)
+function verMensagens(info) {
+    for (let i = 0; i < info.data.length; i++) {
         chat.innerHTML += 
         `
         <div class="mensagem">
-            (${mensagem.data[i].time}) ${mensagem.data[i].from} ${mensagem.data[i].text} ${mensagem.data[i].type}
+            <span class="hora">(${info.data[i].time})</span> <span class="pessoa">${info.data[i].from}</span>  ${info.data[i].text} ${info.data[i].type}
         </div>
         `
     }
-
-    if (mensagem.data[i].type.includes('status')) {
-        tipo.classList.add("status")
-    } else if (mensagem.data[i].type === 'private_message') {
-        tipo.classList.add("reservado")
-    } else {
-        tipo.classList.add("normal")
-    }
+    chat.scrollIntoView()
 }
 
+let mensagemRecebida
+let mensagemEnviada
+let enviarMensagens
+
+function mensagemNoChat(msg) {
+    mensagemRecebida = document.querySelector("input").value
+    mensagemEnviada = {from: nome, to: nome, text: mensagemRecebida, type: "message"}
+    enviarMensagens = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", mensagemEnviada);
+    enviarMensagens.then(mensagemNoChat)
+    enviarMensagens.catch(erroNaMensagem)
+    //console.log(msg.data)
+}
+
+enviarMensagens.then(mensagemNoChat)
+enviarMensagens.catch(erroNaMensagem)
+
+function erroNaMensagem(error) {
+    //console.log(error.data)
+}
