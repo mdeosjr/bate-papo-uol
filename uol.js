24841a0d7bbe53b6e8bef1284d66b70120080ea7
Moises Junior <m.junior@eq.ufrj.br>
2021-11-13 17:35:12 -0300

    Projeto funcional, faltando poucos ajustes

--- uol.js
+++ uol.js
@@ -1,8 +1,17 @@
+let nome 
+
 function entrarSala() {
-    let nome = prompt("Qual é o seu lindo nome?");
+    nome = prompt("Qual é o seu lindo nome?");
+
     const dados = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", { name: nome });
 
-    dados.catch(entrarSala);
+    dados.then(verificarConexao);
+    dados.catch(erroEntrada);
+}
+
+function erroEntrada(info) {
+    alert("Erro: " + info.response.status + ". Nome já em uso, por favor digite outro nome!")
+    entrarSala();
 }
 
 entrarSala();
@@ -13,13 +22,17 @@ function verMensagens(mensagem) {
     const chat = document.querySelector(".chat");
 
     for (let i = 0; i < mensagens.length; i++) {
-        const recebido = mensagens[i];                                                                          
-        chat.innerHTML += `
-        <div class="mensagem"> 
-            <span class="hora">${recebido.time}</span> <span class="pessoa">${recebido.from}</span> ${recebido.text};
+        let recebido = mensagens[i];                                                                          
+        chat.innerHTML += 
+        `
+        <div class="mensagem ${recebido.type}"> 
+            <span class="hora">(${recebido.time})</span> <span class="pessoa">${recebido.from}</span> para <span class="pessoa">${recebido.to}:</span> ${recebido.text}
         </div>
         `
     }
+    let todasAsMensagens = document.querySelector(".chat");
+    let ultimaMensagem = todasAsMensagens.lastElementChild;
+    ultimaMensagem.scrollIntoView();
 }
 
 function verErro (erro) {
@@ -36,3 +49,34 @@ function carregarMensagens() {
 
 setInterval(carregarMensagens, 3000);
 
+function propostaMensagem() {
+    let mensagemAEnviar = document.querySelector("input").value;
+    let objetoMensagens = {
+        from: nome,
+        to: "Todos",
+        text: mensagemAEnviar,
+        type: "message"
+    };
+    const dados = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", objetoMensagens)
+
+    dados.then(enviarMensagem);
+    dados.catch(verErro);
+
+    mensagemAEnviar = '';
+}
+
+function enviarMensagem(mensagem) {
+    const chat = document.querySelector(".chat");
+    chat.innerHTML += 
+    `
+    <div class="mensagem"> 
+        <span class="hora">(${mensagem.data.time})</span> <span class="pessoa">${mensagem.data.from}</span> <span class="pessoa">${mensagem.data.to}</span>: ${mensagem.data.text}
+    </div>
+    `
+}
+
+function verificarConexao () {
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nome })
+}
+
+setInterval(verificarConexao, 5000);
